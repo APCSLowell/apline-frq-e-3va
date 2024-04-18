3242fb1c364c1a62d1826500c5b59ba1500a97db
e-3va <142951737+e-3va@users.noreply.github.com>
2024-04-18 16:52:54 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -3,15 +3,15 @@ public class APLine
   /* your code here */
   private int a, b, c;
   public APLine(int a, int b, int c) {
-    this.a = a;
-    this.b = b;
-    this.c = c;
+    a = a;
+    b = b;
+    c = c;
   }
   public double getSlope() {
-    return ( - ( a / (double) b ));
+    return - a / (double) b;
   }
   public boolean isOnline(int x, int y) {
-    if ((this.a * x) + (this.b * y) + this.c == 0)
+    if ((a * x) + (b * y) + c == 0)
       return true;
     else return false;
   }
