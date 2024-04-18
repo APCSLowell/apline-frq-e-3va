ce95cb5dc30b17333522ba491155cf51afa1a73b
e-3va <142951737+e-3va@users.noreply.github.com>
2024-04-18 15:48:42 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -8,7 +8,7 @@ public class APLine
     c = c;
   }
   public double getSlope() {
-    return - ( a / (double) b );
+    return - ( a / ((double) b ));
   }
   public boolean isOnline(int x, int y) {
     return a * x + b * y + c == 0;
